ac3f8c544b635535bccb6bbe1c997b2164bea9e7
miaoyu <miaoyu@ifun.com>
2023-04-23 11:53:01 +0800

    feat: remove code

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>首页</div>
   <NuxtLayout>
     <NuxtPage keepalive include="index" ></NuxtPage>
   </NuxtLayout>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,5 @@
 <script setup lang="ts">
-    import MovieCard from '~~/components/MovieCard.vue';
-    import { APIResponse } from '~~/types/APIResponse';
 
-    const searchTerm = ref('');
-    const debouncedSearchTerm = refDebounced(searchTerm, 700);
-
-    
-    const page = ref(1);
-    const disabledPrevious = computed(() => {
-        return page.value === 1;
-    })
-    const disabledNext = computed(() => {
-        return page.value + 1 === data.value?.total_pages;
-    })
-    
-    const url = computed(() => {
-        return `api/movies/search?query=${debouncedSearchTerm.value}&page=${page.value}` // Petición en server Nuxt por la busqueda y página
-    });
-
-    const { data } = await useFetch<APIResponse>(url)
 </script>
 
 <template>
